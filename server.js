2f44f3f62c2964970dd74052a0e643b0cbd0d30a
Max Shengelia / Deefling <mas5627@rit.edu>
2022-10-08 11:22:20 +0200

    added API method templates & cyclic config

--- server.js
+++ server.js
@@ -5,7 +5,27 @@ const server = express();
 server.listen(3000, function() {
     console.log('listening on 3000')
 
-    server.get('/', (req, res) => {
-        res.send('Hello World');
+    server.post('/endpoint', (req, res) => {
+        //can be triggered by HTML form or JS AJAX/JS fetch
+        //form action attribute will be the endpoint in params
+        console.log('This is the template for a CREATE operation');
     })
+
+    server.get('/endpoint', (req, res) => {
+        res.send('This is the template for a READ operation');
+    })
+
+    server.put('/endpoint', (req, res) => {
+        //can be triggered by HTML form or JS AJAX/JS fetch
+        //form action attribute will be the endpoint in params
+        console.log('This is the template for an UPDATE operation');
+    })
+
+    server.delete('/endpoint', (req, res) => {
+        //can be triggered by HTML form or JS AJAX/JS fetch
+        //form action attribute will be the endpoint in params
+        console.log('This is the template for an DELETE operation');
+    })
+
+
 })
