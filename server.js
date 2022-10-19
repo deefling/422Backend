8cb97e5cb7c1bf98e48b1626f7e00268592d3a9f
Max Shengelia / Deefling <mas5627@rit.edu>
2022-10-19 12:03:09 +0200

    added DB sample call. need to change from sample data to our data. need to rework DB functionality into diff class.

--- server.js
+++ server.js
@@ -1,9 +1,74 @@
-const { application } = require('express');
 const express = require('express');
 const server = express();
 
-server.listen(3000, function() {
-    console.log('listening on 3000')
+const { MongoClient } = require('mongodb');
+const MONGO_CONNECTION_STRING = "mongodb+srv://root:TargaryensFTW@422databse.axyczfl.mongodb.net/?retryWrites=true&w=majority";
+const uri = MONGO_CONNECTION_STRING;
+const client = new MongoClient(uri);
+
+
+
+
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://root:<password>@422databse.axyczfl.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+
+//figure out how to use this as template
+// app.get("/items/:my_item", async (req, res) => {
+//     let my_item = req.params.my_item;
+//     let item = await client.db("my_db")
+//                 .collection("my_collection")
+//                 .findOne({my_item: my_item})
+
+//     return res.json(item)
+// })
+
+
+//CALLBACKS ARE DEPRECATED
+// figure out how to do the same thing with async/await/promises
+// client.connect(err => {
+//     if(err){ console.error(err); return false;}
+//     // connection to mongo is successful, listen for requests
+//     server.listen(3000, function() {
+//         console.log('listening on 3000')
+//     })
+// });
+
+
+async function bootDB(){
+    try{
+        console.log("test0");
+        await client.connect();
+        console.log("test1");
+        const db = client.db("sample_training");
+        console.log("test2");
+        const collection = db.collection('grades');
+        console.log("test3");
+        const findResult = await collection.find({}).toArray();
+        console.log("test4");
+        console.log('Found documents =>', findResult);
+    } catch (e) {
+    console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+server.listen(3000, api());
+    
+function api() {
+    console.log('API has been booted up');
+    bootDB();
 
     server.post('/endpoint', (req, res) => {
         //can be triggered by HTML form or JS AJAX/JS fetch
@@ -27,5 +92,4 @@ server.listen(3000, function() {
         console.log('This is the template for an DELETE operation');
     })
 
-
-})
+}
