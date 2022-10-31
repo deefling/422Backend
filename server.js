9d98ebdf47c677a57be6de50c91363a8a3340ebe
Max Shengelia / Deefling <mas5627@rit.edu>
2022-10-31 15:50:48 +0100

    still playing with API car functions, added image to test pulling img from server

--- server.js
+++ server.js
@@ -3,6 +3,55 @@ const server = express();
 
 const mongoDriver = require('./mongoDriver');
 
+const CarJSONobj = {cars: [
+    {
+        id: "1", 
+        image: "LINK",
+        car_name: {
+            model: "Civic",
+            brand: "Honda",
+            year: "1997"
+        }
+    },
+    {
+        id: "2", 
+        image: "LINK",
+        car_name: {
+            model: "Cybertruck",
+            brand: "Tesla",
+            year: "2023"
+        }
+    },
+    {
+        id: "3", 
+        image: "LINK",
+        car_name: {
+            model: "P1",
+            brand: "McLaren",
+            year: "2013"
+        }
+    },
+    {
+        id: "4", 
+        image: "LINK",
+        car_name: {
+            model: "Silver Ghost",
+            brand: "Rolls Royce",
+            year: "1925"
+        }
+    },
+    {
+        id: "5", 
+        image: "LINK",
+        car_name: {
+            model: "Aventador",
+            brand: "Lamborghini",
+            year: "2018"
+        }
+    }
+]};
+
+
 server.listen(3000, api());
     
 function api() {
@@ -19,8 +68,21 @@ function api() {
         res.send('This is the template for a READ operation');
     })
 
+    server.get('/getCars', (req, res) => { 
+        res.json(CarJSONobj);
+    })
+
+    server.get('/getFeaturedCars', (req, res) => {
+        var featuredCars = {cars:[]};
+        featuredCars['cars'].push(CarJSONobj['cars'][1]);
+        featuredCars['cars'].push(CarJSONobj['cars'][0]);
+        featuredCars['cars'].push(CarJSONobj['cars'][4]);
+        res.json(featuredCars);
+    })
+
     server.get('/getLogins', (req, res) => {
         // HARDCODED
+        //I plan to change this to take login creds & return bool rather than exposing passwords
         JSONobj = {stuff: [
             {
             id: "1", 
