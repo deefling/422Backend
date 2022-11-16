3a5191bff6ef0089d3a10022c6fd82556a25802e
Max Shengelia / Deefling <mas5627@rit.edu>
2022-11-16 09:46:54 +0100

    worked on updateCar

--- scripts/fillTables.js
+++ scripts/fillTables.js
@@ -26,17 +26,19 @@ async function run(){
     await mongoDriver.addCarType("Luxury Car");
 
     //fill model with sample data
-    await mongoDriver.addModel("Civic", 9, 2);
+    await mongoDriver.addModel("Civic", 9, 1);
     await mongoDriver.addModel("Cybertruck", 5, 0);
     await mongoDriver.addModel("P1", 6, 3);
     await mongoDriver.addModel("Silver Ghost", 3, 4);
     await mongoDriver.addModel("Aventador", 4, 3);
+    await mongoDriver.addModel("Accord", 9, 1);
+    await mongoDriver.addModel("Breeze", 9, 0);
 
     //fill year with sample data
-    await mongoDriver.addModelYear(0, "1997", "/images/civicMain.jpg", "/images/civicHeader.jpg", "Newer civic", false, 13);
+    await mongoDriver.addModelYear(0, "1997", "/images/civicMain.jpg", "/images/civicHeader.jpg", "Newer civic", true, 13);
     await mongoDriver.addModelYear(1, "2020", "/images/cybertruckMain.jpg", "/images/cybertruckHeader.jpg", "Older cybertruck", false, 25);
-    await mongoDriver.addModelYear(3, "2013", "/images/p1Main.jpg", "/images/p1Header.jpg", "P1 cleaner than your church shoes", false, 5);
-    await mongoDriver.addModelYear(4, "1925", "/images/silverGhostMain.jpg", "/images/silverGhostHeader.jpg", "If you hear clanking in the engine of a Rolls Royce...get your ears checked", false, 7);
+    await mongoDriver.addModelYear(2, "2013", "/images/p1Main.jpg", "/images/p1Header.jpg", "P1 cleaner than your church shoes", true, 5);
+    await mongoDriver.addModelYear(3, "1925", "/images/silverGhostMain.jpg", "/images/silverGhostHeader.jpg", "If you hear clanking in the engine of a Rolls Royce...get your ears checked", true, 7);
     await mongoDriver.addModelYear(4, "2018", "/images/aventadorMain.jpg", "/images/aventadorHead.jpg", "Has scissor doors", false, 33);
     // console.log(await mongoDriver.getModelYears());
 
